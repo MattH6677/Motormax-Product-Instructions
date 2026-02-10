da38fb61afd7e14d657b6c7a84dace47bbcf4a04
MattH6677 <matt.h@motormax.co.uk>
2026-02-10 10:36:47 +0000

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -1,1 +1,9 @@
+document.querySelectorAll('.dropdown-toggle').forEach(button => {
+  button.addEventListener('click', () => {
+    const dropdown = button.parentElement;
+    const expanded = button.getAttribute('aria-expanded') === 'true';
 
+    button.setAttribute('aria-expanded', !expanded);
+    dropdown.classList.toggle('open');
+  });
+});
